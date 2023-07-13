7eaae86bd14739c906f1e8d610fd2dd1b08f735f
Tanagra04 <vanegasrojas29@gmail.com>
2023-07-12 21:14:52 -0500

    12072301

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+/**
+ * para declarar variables var, let, const
+ * tipos datos: null, undefined, string(texto), bolean(true, false),number, symbol y object
+ */
+
+let miEdad=47;//tipo numero
+
+const miNombre = 'Andrea'; //tipo texto
+
+const casado = false; //tipo boleano
+
+// miNombre = 'Manuela';
+
+console.log('Mi nombre: ' + miNombre)
+console.log('Mi edad: ' + miEdad)
+console.log('Estado Civil: ' + casado)

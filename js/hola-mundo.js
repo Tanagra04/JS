1be8f968a45d84094264b78b14e0bb6ca5e6fb8c
Tanagra04 <vanegasrojas29@gmail.com>
2023-07-12 20:51:15 -0500

    primera app

---
+++ js/hola-mundo.js
@@ -0,0 +1,7 @@
+// primera app en javascript
+/**
+ * app hola mundo utiliza una funcion llamada console.log() 
+ */
+
+//muestra el resultado en la consola del navegador
+console.log('Hola Mundo');

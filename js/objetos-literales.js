5db80b9b1cd3350233c3c7ac10e9d674a1622b58
Tanagra04 <vanegasrojas29@gmail.com>
2023-07-14 20:31:47 -0500

    14072303

--- js/objetos-literales.js
+++ js/objetos-literales.js
@@ -12,3 +12,16 @@ console.table(libros);
 
 //imprime el libro crimen y castigo
 console.log('El libro es: ', libros.nombre);
+//imprime el libro El principito
+console.log('El libro es: ', libros.nombre2);
+//consultar el elemento precio1 del objeto
+const p1 = 'precio1';
+console.log('El precio 1 es: ', libros[p1]);
+
+//agregar nueva propiedad al objeto libros
+libros.nuevoLibro = 'Una mujer de cuatro en conducta'
+console.log(libros);
+
+// eliminar una propiedad del objeto libros
+delete libros.nombre;
+console.log(libros);

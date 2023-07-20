df92a6e1b8635d229a5dff6f1af9d0e6448dfc4f
Tanagra04 <vanegasrojas29@gmail.com>
2023-07-19 20:05:30 -0500

    190720

---
+++ js/condicionales.js
@@ -0,0 +1,12 @@
+//if y else, determina si la condicion se cumple o no
+//ejemplo1: Mostrar el nombre del profe de frontend
+
+let nombreMateria = 'frontend';
+
+if(nombreMateria==='frontend') {
+    //mostrar nombre dle profe
+    console.log(`Nombre del profe: Jorge - ${nombreMateria}`);
+
+}else{
+    console.log('No es el nombre del profe');
+}
